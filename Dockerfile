a5f2cd3863c3e5bf771fcdbda11934b904ae0eec
n_dovronbekov <N.DOVRONBEKOV@uzinfocom.uz>
2025-09-09 21:02:00 +0500

    first commitf

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
-FROM golang:1.22-alpine AS builder
+# Go build stage
+FROM golang:1.24.6-alpine AS builder
+
 WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
 COPY . .
+
 RUN go build -o server server.go
 
+# Final stage
 FROM alpine:3.20
 WORKDIR /app
 COPY --from=builder /app/server .
--- server.go
+++ server.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"runtime"
 	"time"
+
+	"github.com/shirou/gopsutil/v3/cpu"
 )
 
 func main() {
@@ -13,9 +17,31 @@ func main() {
 	})
 
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		// Oddiy metrikaning demo varianti
 		now := time.Now().Unix()
-		fmt.Fprintf(w, "app_up 1\napp_time %d\n", now)
+
+		// RAM metrikalari
+		var m runtime.MemStats
+		runtime.ReadMemStats(&m)
+
+		// CPU foizi
+		percent, _ := cpu.Percent(0, false) // 0 = instant foiz, false = umumiy CPU
+		cpuUsage := 0.0
+		if len(percent) > 0 {
+			cpuUsage = percent[0]
+		}
+
+		// PID
+		pid := os.Getpid()
+
+		// Prometheus formatidagi metrikalar
+		fmt.Fprintf(w, "app_up 1\n")
+		fmt.Fprintf(w, "app_time %d\n", now)
+		fmt.Fprintf(w, "app_pid %d\n", pid)
+		fmt.Fprintf(w, "app_alloc_bytes %d\n", m.Alloc)
+		fmt.Fprintf(w, "app_total_alloc_bytes %d\n", m.TotalAlloc)
+		fmt.Fprintf(w, "app_sys_bytes %d\n", m.Sys)
+		fmt.Fprintf(w, "app_num_gc %d\n", m.NumGC)
+		fmt.Fprintf(w, "app_cpu_percent %.2f\n", cpuUsage) // CPU foizda
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
